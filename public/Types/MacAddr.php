ecacfbb3620563f465041f03544ce02bcaf5e35a
A young deveoloper <ayoungdevoloper@gmail.com>
2017-07-29 14:15:02 +0300

    added mac addr type

---
+++ public/Types/MacAddr.php
@@ -0,0 +1,13 @@
+<?php
+namespace Cable\Ordm\Types;
+
+/**
+ * Class MacAddr
+ * @package Cable\Ordm\Types
+ */
+class MacAddr extends StringType
+{
+    protected $name = 'mac_addr';
+
+    // do nothing
+}
