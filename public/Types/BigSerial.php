b8626542f47b412bcf30051495834c36de7ffcaa
A young deveoloper <ayoungdevoloper@gmail.com>
2017-07-29 14:47:02 +0300

    added some types

---
+++ public/Types/BigSerial.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cable\Ordm\Types;
+
+/**
+ * Class BigSerial
+ * @package Cable\Ordm\Types
+ */
+class BigSerial extends Integer
+{
+
+    /**
+     * @var string
+     */
+    protected $name = 'bigserial';
+}
+
